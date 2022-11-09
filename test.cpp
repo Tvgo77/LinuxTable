26fd928eaa8a682851fd05e497ab0d8693819845
Tvgo77 <tvgoshao77@gmail.com>
2022-11-09 20:46:00 +0800

    nothing

--- test.cpp
+++ test.cpp
@@ -1,11 +1,6 @@
-#include "bpt1.h"
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include "table_api.h"
 
 
 int main() {
-    int a[5] = {1, 3, 5, 7, 9};
-    void *p = std::lower_bound(std::begin(a), std::end(a), 0);
-    return 0;
+    ;
 }
